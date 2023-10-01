75c30563c6e1b8a577fb301d65c664455634849a
faisal <faisal.mahboob@hotmail.com>
2023-10-01 22:38:09 +1100

    Playing with React To Do App

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,7 +4,21 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  function handlesubmit(e) {
+    e.preventDefault();
+    let todo = document.getElemenetById('todoAdd').value
+    const newToDo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newToDo.text.length > 0) {
+      setToDos([...todos].concat(newToDo));
+    } else {
+      alert ("Enter a Valid Task");
+    }
+    document.getElementById('todoAdd').value = ""
+    }
   
   // Add the deleteToDo code here
 
